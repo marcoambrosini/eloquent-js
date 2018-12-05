7498736062c1cac8abe7e262f6e6f20476b5d462
mrc <marcoambrosini@protonmail.com>
2018-12-05 13:56:00 +0100

    012 Your own loop

---
+++ 012 Your own loop/yourownloop.js
@@ -0,0 +1,7 @@
+/* Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning. */
+
+function loop(value, test, update, body) {
+    for (value ; test(value) ; update(value)) {
+            body(value);
+        } 
+    }
