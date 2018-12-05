7eb5d1364fb21c5c250f4e1647921648e54f4633
mrc <marcoambrosini@protonmail.com>
2018-12-05 10:15:49 +0100

    009 A list

--- 009 A list/alist.js
+++ 009 A list/alist.js
@@ -10,9 +10,55 @@ let list = {
     }
   }
 }; */
+let examplelist = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
 
 function arrayToList(arr) {
-  
-        let list = {'value' : arr[arr.length], 'rest' = } 
-    
+  let list = null;
+  for(n=arr.length; n > 0; n--) {
+    list = {'value' : arr[n], rest : list};
+  }
+  return list;
 }
+
+
+
+let newarr = [];
+function listToarray(list) {
+  newarr.push(list.value);
+  if (list.rest != null) {
+  listToarray(list.rest);
+  } else return newarr;
+}
+
+listToarray(examplelist);
+
+
+function prepend(list, element) {
+  let newlist = {value: element, rest:list};
+  return newlist;
+}
+
+prepend(examplelist, 7);
+
+function nth(list, num) {
+  for (let node = list; node; node = node.rest) {
+    if (num === 0) {
+      return node;
+    } else {
+      num -= 1;
+      nth (node, num);
+  }
+  }
+}
+
+nth(examplelist, 2);
